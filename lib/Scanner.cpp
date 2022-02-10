7797105419f490529c42a16da997b42be2f2e9e6
Siddharth <siddhartha.pawar17@gmail.com>
2022-02-10 15:19:23 +0530

    Single character token scannable

--- lib/Scanner.cpp
+++ lib/Scanner.cpp
@@ -41,7 +41,20 @@ char Scanner::advance()
 
 void Scanner::scanToken()
 {
+    char c = advance();
 
+    switch (c) {
+        case '(': addToken(TokenType::LEFT_PAREN); break;
+        case ')': addToken(TokenType::RIGHT_PAREN); break;
+        case '{': addToken(TokenType::LEFT_BRACE); break;
+        case '}': addToken(TokenType::RIGHT_BRACE); break;
+        case ',': addToken(TokenType::COMMA); break;
+        case '.': addToken(TokenType::DOT); break;
+        case '-': addToken(TokenType::MINUS); break;
+        case '+': addToken(TokenType::PLUS); break;
+        case ';': addToken(TokenType::SEMICOLON); break;
+        case '*': addToken(TokenType::STAR); break;
+    }
 }
 
 bool Scanner::isAtEnd()
