79a90a14878ef5482b264387a881f9d44fd08760
Siddharth <siddhartha.pawar17@gmail.com>
2022-03-03 17:21:21 +0530

    Block statement resolver methods added

--- include/Semantic/Resolver.h
+++ include/Semantic/Resolver.h
@@ -1,27 +1,43 @@
 #pragma once
 
+#include <stack>
+#include <unordered_map>
+
 #include "./../Parser/Expression/ExpressionHeaders.h"
 #include "./../Parser/Stmt/StmtHeaders.h"
 #include "./../Interpreter/Interpreter.h"
 
 class Resolver: 
-public Expr::Visitor<void*>,
+public Expr::Visitor<std::string*>,
 public Stmt::Visitor<void*>
 {
+    public:
+        // Each element in stack represents a single block scope.
+        // Scope stack is only for local block scopes
+        // Global scope is not tracked by resolver
+        std::stack<std::unordered_map<std::string, bool>*>* scopes;
+
     public: 
         Interpreter* interpreter;
 
     public:
         Resolver(Interpreter* interpreter);
 
+    // public:
+
+
     public:
+        virtual void* visitVarStmt(Stmt::Var* stmt) override;
         virtual void* visitBlockStmt(Stmt::Block* stmt) override;
 
     private:
         void beginScope();
         void endScope();
         void resolve(Stmt::Stmt* statement);
+        void resolve(Expr::Expr* statement);
         void resolve(std::vector<Stmt::Stmt*>* statements);
+        void declare(Token* name);
+        void define(Token* name);
 };
 
 /*
--- lib/Semantic/Resolver.cpp
+++ lib/Semantic/Resolver.cpp
@@ -3,7 +3,23 @@
 Resolver::Resolver(Interpreter* interpreter)
 {
     this->interpreter = interpreter;
+
+    scopes = new std::stack<std::unordered_map<std::string, bool>*>();
 }
+
+
+void* Resolver::visitVarStmt(Stmt::Var* stmt)
+{
+    // Binding done in two steps: Declaring and Defining 
+
+    // declare(stmt->name);
+    if (stmt->initializer != nullptr) {
+        resolve(stmt->initializer);
+    }
+    // define(stmt->name);
+    return nullptr;
+}
+
 
 void* Resolver::visitBlockStmt(Stmt::Block* stmt)
 {
@@ -16,22 +32,50 @@ void* Resolver::visitBlockStmt(Stmt::Block* stmt)
 
 void Resolver::beginScope()
 {
-
+    scopes->push(new std::unordered_map<std::string, bool>());
 }
 
 void Resolver::endScope()
 {
-
+    scopes->pop();
 }
 
-void Resolver::resolve(Stmt::Stmt* statements)
+void Resolver::resolve(Stmt::Stmt* statement)
+{
+    statement->accept(this);
+}
+
+void Resolver::resolve(Expr::Expr* statement)
+{
+    statement->accept(this);
+}
+
+void Resolver::resolve(std::vector<Stmt::Stmt*>* statements)
 {
     for (Stmt::Stmt* statement: *statements) {
         resolve(statement);
     }
 }
 
-void Resolver::resolve(std::vector<Stmt::Stmt*>* statements)
+void Resolver::declare(Token* name)
 {
+    if (scopes->empty()) {
+        return;
+    }
 
+    std::unordered_map<std::string, bool>* scope = scopes->top();
+
+    // Boolean related to string represents whether the variable
+    // has finished resolving the initializer
+    (*scope)[*name->lexeme] = false;
+}
+
+void Resolver::define(Token* name)
+{
+    if (scopes->empty()) {
+        return;
+    }
+
+    // Variable fully initialised and available for use
+    (*scopes->top())[*name->lexeme] = true;
 }
